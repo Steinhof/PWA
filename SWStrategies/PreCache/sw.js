4fee023fb491b8afe1631c33e52486d5adb09a6b
Steinhof <steinhof808@gmail.com>
2019-09-15 20:52:49 +0300

    PreCache strategy for sw

---
+++ SWStrategies/PreCache/sw.js
@@ -0,0 +1,48 @@
+const VERSION = '1.07';
+const CACHE_NAME = `PreCache-${VERSION}`;
+
+const CACHED_URLS = [
+    '/',
+    'css/main.css',
+    'img/cat.webp'
+];
+
+
+/*  Add files to site cache */
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(CACHED_URLS);
+            }));
+    console.log('service worker installed');
+});
+
+
+/*  Remove old files from cache */
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.keys()
+            .then(cacheNames => {
+                return Promise.all(
+                    cacheNames.map((item) => {
+                        if (CACHE_NAME !== item) {
+                            return caches.delete(item);
+                        }
+                    })
+                );
+            })
+    );
+    console.log('service worker activated');
+});
+
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function (response) {
+                return response || fetch(event.request);
+            })
+    );
+    console.log('fetch complete');
+});
